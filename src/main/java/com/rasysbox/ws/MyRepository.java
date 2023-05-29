3fb6ffebe155d1f70ff7cad49e5e76b1d39b4f65
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-05-29 13:18:46 -0500

    Refactor Test #3

--- src/main/java/com/rasysbox/ws/MyRepository.java
+++ src/main/java/com/rasysbox/ws/MyRepository.java
@@ -2,7 +2,11 @@ package com.rasysbox.ws;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class MyRepository {
@@ -14,13 +18,30 @@ public class MyRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void executeQuery() {
-        String sql = "SELECT * FROM my_table";
-        jdbcTemplate.query(sql, (rs, rowNum) -> {
-            // Procesar resultados de la consulta
-            // rs.getString("column_name");
-            return null;
-        });
+    public String executeQuery(String msisdn) {
+        String sql = "SELECT a.msisdn, b.ido AS ido_receiver " +
+                "FROM spnv_col.spn_numeration a " +
+                "JOIN spnv_col.spn_operator b ON a.receiverop = b.operatorid " +
+                "JOIN spnv_col.spn_operator c ON a.donorop = c.operatorid " +
+                "JOIN spnv_col.spn_operator d ON a.ownerop = d.operatorid " +
+                "WHERE a.msisdn = :msisdn";
+        StringBuilder result = new StringBuilder();
+        jdbcTemplate.query(sql, new ResultSetExtractor<Void>() {
+            @Override
+            public Void extractData(ResultSet rs) throws SQLException {
+                while (rs.next()) {
+                    String msisdnValue = rs.getString("msisdn");
+                    String idoValue = rs.getString("ido_receiver");
+
+                    result.append("msisdn: ").append(msisdnValue)
+                            .append(", ido_receiver: ").append(idoValue)
+                            .append("\n");
+                }
+                return null;
+            }
+        }, msisdn);
+
+        return result.toString();
     }
 
 }
